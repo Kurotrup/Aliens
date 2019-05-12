0b00339c9c9be307c3b768b1a5c4fbbeb6ef6619
kur <kur@localhost.localdomain>
2019-05-12 18:33:41 +0300

    Created 2 addiditional func in game_func.py due to refactoring of check_events func. check_kd_events for process whith key down events. check_ku_events for process whith key up events.

--- game_func.py
+++ game_func.py
@@ -2,27 +2,35 @@ import sys
 import pygame
 
 
+def check_kd_events(event, ship):
+    """reacted on the button pushing"""
+    if event.key == pygame.K_RIGHT:
+        ship.move_right = True
+    elif event.key == pygame.K_LEFT:
+        ship.move_left = True
+    elif event.key == pygame.K_LSHIFT:
+        ship.ai_param.ship_sp_f *= 10
+
+
+def check_ku_events(event, ship):
+    """ reacted on the button releaving """
+    if event.key == pygame.K_RIGHT:
+        ship.move_right = False
+    elif event.key == pygame.K_LEFT:
+        ship.move_left = False
+    elif event.key == pygame.K_LSHIFT:
+        ship.ai_param.ship_sp_f /= 10
+
+
 def check_events(ship):
     """ process buttoms clics and mouse moves """
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
         elif event.type == pygame.KEYDOWN:
-            # move ship
-            if event.key == pygame.K_RIGHT:
-                ship.move_right = True
-            elif event.key == pygame.K_LEFT:
-                ship.move_left = True
-            elif event.key == pygame.K_LSHIFT:
-                ship.ai_param.ship_sp_f *= 10
+            check_kd_events(event, ship)
         elif event.type == pygame.KEYUP:
-            # stop move ship
-            if event.key == pygame.K_RIGHT:
-                ship.move_right = False
-            elif event.key == pygame.K_LEFT:
-                ship.move_left = False
-            elif event.key == pygame.K_LSHIFT:
-                ship.ai_param.ship_sp_f /= 10
+            check_ku_events(event, ship)
 
 
 def update_screen(screen, ship, bg):
